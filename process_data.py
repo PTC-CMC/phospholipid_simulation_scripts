e58f78857c42bd090ec03d2b99b1b2e44ec15e9b
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-06-12 15:04:38 -0500

    Use uncertainties for more reliable error propagation

--- process_data.py
+++ process_data.py
@@ -2,6 +2,8 @@ import os
 import json
 import numpy as np
 from collections import OrderedDict
+from uncertainties import ufloat
+from uncertainties.umath import *
 import pandas as pd
 import pdb
 ################
@@ -10,9 +12,11 @@ import pdb
 
 index = json.load(open('index.txt','r'))
 curr_dir = os.getcwd()
-df = pd.DataFrame()
+summary_df = pd.DataFrame()
+all_df = pd.DataFrame()
 all_compositions = [{'DSPC':32, 'ffa12':32}, {'DSPC':32, 'ffa16':32}, 
-                    {'DSPC':32, 'ffa24':32} ]
+        {'DSPC':32, 'ffa24':32}, {'DSPC':32, 'oh12':32}, {'DSPC':32, 'oh16':32},
+        {'DSPC':32, 'oh24':32}]
 for composition_i in all_compositions:
     # Initialize data structures
     composition_data = {}
@@ -80,32 +84,70 @@ for composition_i in all_compositions:
                 composition_data[component + "_offset_std"].append(
                                         sim_data['Offset'][component]['std'])
                 composition_data[component + "_offset_unit"] = sim_data['Offset']['unit']
-
+    all_df = all_df.append(composition_data, ignore_index=True)
 
     # Now compute averages and propagate error
-    composition_data['APL_mean'] = np.mean(composition_data['APL_mean'])
-    composition_data['APL_std'] = np.sqrt(sum(err**2 for err in composition_data['APL_std']))
+    ufloats = [ufloat(val,std) for val,std in zip(composition_data['APL_mean'], 
+                                                  composition_data['APL_std'])]
+    foo = np.mean(ufloats)
+    composition_data['APL_mean'] = foo.n
+    composition_data['APL_std'] = foo.s
 
-    composition_data['APT_mean'] = np.mean(composition_data['APT_mean'])
-    composition_data['APT_std'] = np.sqrt(sum(err**2 for err in composition_data['APT_std']))
+    #composition_data['APL_mean'] = np.mean(composition_data['APL_mean'])
+    #composition_data['APL_std'] = np.sqrt(sum(err**2 for err in composition_data['APL_std']))
 
-    composition_data['H_mean'] = np.mean(composition_data['H_mean'])
-    composition_data['H_std'] = np.sqrt(sum(err**2 for err in composition_data['H_std']))
+    ufloats = [ufloat(val,std) for val,std in zip(composition_data['APT_mean'], 
+                                                  composition_data['APT_std'])]
+    foo = np.mean(ufloats)
+    composition_data['APT_mean'] = foo.n
+    composition_data['APT_std'] = foo.s
+    #composition_data['APT_mean'] = np.mean(composition_data['APT_mean'])
+    #composition_data['APT_std'] = np.sqrt(sum(err**2 for err in composition_data['APT_std']))
 
-    composition_data['Tilt_mean'] = np.mean(composition_data['Tilt_mean'])
-    composition_data['Tilt_std'] = np.sqrt(sum(err**2 for err in composition_data['Tilt_std']))
+    ufloats = [ufloat(val,std) for val,std in zip(composition_data['H_mean'], 
+                                                  composition_data['H_std'])]
+    foo = np.mean(ufloats)
+    composition_data['H_mean'] = foo.n
+    composition_data['H_std'] = foo.s
+    #composition_data['H_mean'] = np.mean(composition_data['H_mean'])
+    #composition_data['H_std'] = np.sqrt(sum(err**2 for err in composition_data['H_std']))
 
-    composition_data['S2_mean'] = np.mean(composition_data['S2_mean'])
-    composition_data['S2_std'] = np.sqrt(sum(err**2 for err in composition_data['S2_std']))
+    ufloats = [ufloat(val,std) for val,std in zip(composition_data['Tilt_mean'], 
+                                                 composition_data['Tilt_std'])]
+    foo = np.mean(ufloats)
+    composition_data['Tilt_mean'] = foo.n
+    composition_data['Tilt_std'] = foo.s
+    #composition_data['Tilt_mean'] = np.mean(composition_data['Tilt_mean'])
+    #composition_data['Tilt_std'] = np.sqrt(sum(err**2 for err in composition_data['Tilt_std']))
 
-    composition_data['Idig_mean'] = np.mean(composition_data['Idig_mean'])
-    composition_data['Idig_std'] = np.sqrt(sum(err**2 for err in composition_data['Idig_std']))
+    ufloats = [ufloat(val,std) for val,std in zip(composition_data['S2_mean'], 
+                                                  composition_data['S2_std'])]
+    foo = np.mean(ufloats)
+    composition_data['S2_mean'] = foo.n
+    composition_data['S2_std'] = foo.s
+    #composition_data['S2_mean'] = np.mean(composition_data['S2_mean'])
+    #composition_data['S2_std'] = np.sqrt(sum(err**2 for err in composition_data['S2_std']))
+
+    ufloats = [ufloat(val,std) for val,std in zip(composition_data['Idig_mean'], 
+                                                  composition_data['Idig_std'])]
+    foo = np.mean(ufloats)
+    composition_data['Idig_mean'] = foo.n
+    composition_data['Idig_std'] = foo.s
+    #composition_data['Idig_mean'] = np.mean(composition_data['Idig_mean'])
+    #composition_data['Idig_std'] = np.sqrt(sum(err**2 for err in composition_data['Idig_std']))
 
     for component, number in composition_i.items():
-        composition_data[component + "_offset_mean"] = np.mean(composition_data[component + "_offset_mean"])
-        composition_data[component + "_offset_std"] = np.sqrt(sum(err**2 for err in composition_data[component+'_offset_std']))
+        ufloats = [ufloat(val,std) for val,std in 
+                    zip(composition_data[component + '_offset_mean'], 
+                        composition_data[component + '_offset_std'])]
+        foo = np.mean(ufloats)
+        composition_data[component + '_offset_mean'] = foo.n
+        composition_data[component + '_offset_std'] = foo.s
+        #composition_data[component + "_offset_mean"] = np.mean(composition_data[component + "_offset_mean"])
+        #composition_data[component + "_offset_std"] = np.sqrt(sum(err**2 for err in composition_data[component+'_offset_std']))
 
-    df = df.append(composition_data, ignore_index=True)
+    summary_df = summary_df.append(composition_data, ignore_index=True)
 
 os.chdir(curr_dir)
-df.to_csv('summary.csv')
+summary_df.to_csv('summary.csv')
+all_df.to_csv('all_sims.csv')
