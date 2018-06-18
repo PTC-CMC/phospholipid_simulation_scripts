69e33f1bfd58f71dbe0564bd5ed8422c79a6ed5c
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-06-18 14:03:56 -0500

    Typo in function args

--- run_rwmd.py
+++ run_rwmd.py
@@ -16,7 +16,7 @@ for i, name in enumerate(index.keys()):
     os.chdir(os.path.join(curr_dir, name))
     rwmd_submission = operations.write_rwmd_lines(tc_groups=['non-water', 'water'],
                                 t_pairs=[[305, 455], [305,455]], cooling_rate=1000,
-                                gro='npt_500ps.gro', top='compound.top', 
+                                gro='npt_500ps.gro', top='compound.top')
     with open('rwmd_chain.pbs', 'w') as f:
         body = 'cd {}\n'.format(os.getcwd())
         body += rwmd_submission
